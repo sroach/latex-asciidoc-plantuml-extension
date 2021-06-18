3e0933b082530fa20e87d2723734b10ceea14e28
sroach <sroachst@gmail.com>
2021-06-17 20:43:03 -0400

    init

---
+++ src/main/kotlin/com/roach/asciidoc/extensions/LatexAsciiDocRegistry.kt
@@ -0,0 +1,11 @@
+package com.roach.asciidoc.extensions
+
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.jruby.extension.spi.ExtensionRegistry
+
+class LatexAsciiDocRegistry : ExtensionRegistry {
+    override fun register(asciidoctor: Asciidoctor) {
+        val reg = asciidoctor.javaExtensionRegistry()
+        reg.block(LatexBlockProcessor::class.java)
+    }
+}
---
+++ src/main/kotlin/com/roach/asciidoc/extensions/LatexBlockProcessor.kt
@@ -0,0 +1,44 @@
+package com.roach.asciidoc.extensions
+
+import net.sourceforge.plantuml.FileFormat
+import net.sourceforge.plantuml.FileFormatOption
+import net.sourceforge.plantuml.SourceStringReader
+import org.asciidoctor.ast.ContentModel
+import org.asciidoctor.ast.StructuralNode
+import org.asciidoctor.extension.BlockProcessor
+import org.asciidoctor.extension.Contexts
+import org.asciidoctor.extension.Name
+import org.asciidoctor.extension.Reader
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.charset.Charset
+
+
+@Name("latex")
+@Contexts(Contexts.LISTING)
+@ContentModel(ContentModel.COMPOUND)
+class LatexBlockProcessor :   BlockProcessor(){
+    override fun process(parent: StructuralNode, reader: Reader, attributes: MutableMap<String, Any>): Any? {
+        val content = reader.read()
+
+        val sourceReader = SourceStringReader(content)
+        val filename = attributes.getOrDefault("2", "${System.currentTimeMillis()}_unk")
+
+        val f = File("${reader.dir}/images/$filename.svg")
+        val os = FileOutputStream(f)
+
+        sourceReader.outputImage(os, FileFormatOption(FileFormat.SVG))
+
+        //val desc = reader.generateImage(os, FileFormatOption(FileFormat.SVG))
+        os.close()
+
+        val contentLines = mutableListOf<String>()
+        contentLines.add(".$filename");
+        contentLines.add("image::images/$filename.svg[Interactive,opts=interactive]");
+
+        parseContent(parent, contentLines)
+       // val svg = String(os.toByteArray(), Charset.forName("UTF-8"))
+        return null
+    }
+}
